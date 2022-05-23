9527bed6bc509e12360bf54e545e8f1f44ad4c41
Joe Marshall <joe.marshall@nottingham.ac.uk>
2022-05-23 23:59:12 +0100

    built with new requests

--- build_jupyter.sh
+++ build_jupyter.sh
@@ -4,12 +4,16 @@ if [[ $BASH_SOURCE = */* ]]; then
      cd -- "${BASH_SOURCE%/*}/" || exit
 fi
 echo $PWD
-pip install jupyter-build/override_addon
+pip install -U jupyter-src/override_addon
 mkdir -p jupyter-src/static/pyodide
 mkdir -p docs
 cp ~/pyodide/build/* jupyter-src/static/pyodide/
 rm -rf docs/*
 cd jupyter-src
 rm -rf _output
-jupyter lite build
+JUPYTER_COMMAND=build
+if [ -n "$1" ]; then
+    JUPYTER_COMMAND=$1
+fi
+jupyter lite ${JUPYTER_COMMAND}
 cp -r _output/* ../docs
--- jupyter-src/override_addon/override_addon/__init__.py
+++ jupyter-src/override_addon/override_addon/__init__.py
@@ -9,14 +9,16 @@ class FetchAddon(BaseAddon):
     def post_build(self, manager):
         output_files_dir=manager.output_dir
         src_files_dir=manager.lite_dir / "overrides"
-        print("YAY:",src_files_dir,output_files_dir)
 
         for r,d,f in os.walk(src_files_dir):
             for name in f:
                 src_file=Path(r) / name
                 rel= src_file.relative_to(src_files_dir)
                 dest_file=output_files_dir / rel
-                print("WOO:",src_file,dest_file)
+                if str(dest_file).endswith(".override"):
+                    # some files with special meaning to jupyterlite need ignoring
+                    # so we add .override into the filename
+                    dest_file=Path(str(dest_file)[:-len(".override")])
                 yield dict(
                         name=f"override copy:{rel} {dest_file}",
                         doc=f"override copy {src_file} to {dest_file}",
