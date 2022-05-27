19e758c0d2db054774f8dbad963808ea2ec70a7b
Joe Marshall <joe.marshall@nottingham.ac.uk>
2022-05-27 14:14:25 +0100

    deleted old scripts

--- build_pyodide_modules.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-# to build pyodide modules we need a full build of pyodide currently 
-git submodule init
-cd pyodide
-cp ../pyodide_metafiles/* packages/ -r
-git apply ../patches/pyodide-build.patch 
-./run_docker bash ts-build-packages.sh
-
-
