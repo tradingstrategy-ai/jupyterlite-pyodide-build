27035558a34128fe8022457dbe45268628d19c7a
Joe Marshall <joe.marshall@nottingham.ac.uk>
2022-07-19 14:46:19 +0100

    run docker fixes for github ci

--- run_docker.sh
+++ run_docker.sh
@@ -4,8 +4,14 @@ source envs.sh
 
 DOCKEROPT=-t
 
+if [ -t 0 ] ; then
+    bash ./build_docker.sh
+    DOCKEROPT=-it
+fi
+
+
 docker run  ${DOCKEROPT} -w /home/docker/src --pull==never -v "$PWD":"/home/docker/src"  tradingstrategy:jupyterlite bash -c "cd /home/docker/src && export MAKE=make && cmake . -D PYODIDE_VERSION=${PYODIDE_VERSION} -D PYODIDE_SOURCE=${PYODIDE_SOURCE} -D JUPYTERLITE_VERSION=${JUPYTERLITE_VERSION} -DCMAKE_MAKE_PROGRAM=/usr/bin/make " 
-#docker run  ${DOCKEROPT} -w /home/docker/src --pull==never -v "$PWD":"/home/docker/src"   tradingstrategy:jupyterlite su docker -lc "export MAKE=make && export PYODIDE_JOBS=10 && make -j 10"
-#if [ -t 0 ] ; then
-#  docker run  ${DOCKEROPT} -w /home/docker/src --pull==never -v "$PWD":"/home/docker/src"   tradingstrategy:jupyterlite bash    
-#fi
+docker run  ${DOCKEROPT} -w /home/docker/src --pull==never -v "$PWD":"/home/docker/src"   tradingstrategy:jupyterlite bash -c "export MAKE=make && export PYODIDE_JOBS=10 && make -j 10"
+if [ -t 0 ] ; then
+  docker run  ${DOCKEROPT} -w /home/docker/src --pull==never -v "$PWD":"/home/docker/src"   tradingstrategy:jupyterlite bash    
+fi
