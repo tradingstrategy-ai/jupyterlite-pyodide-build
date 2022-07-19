651fd4ccdd9edcbd283a32885d44f176eacaee39
Joe Marshall <joe.marshall@nottingham.ac.uk>
2022-07-19 10:49:15 +0100

    CI fixes

--- run_docker.sh
+++ run_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -e
 # we piggy back on the pyodide docker image as
 # a) it has everything we need for building 
 # b) it means we don't need to launch a separate docker for pyodide build 
@@ -17,8 +17,8 @@ if [ -t 0 ] ; then
     DOCKEROPT=-it
 fi
 
-docker run  ${DOCKEROPT} -w /home/docker/src --pull==never -v "$PWD":"/home/docker/src"  tradingstrategy:jupyterlite bash -i -c "export MAKE=make && cmake . -D PYODIDE_VERSION=${PYODIDE_VERSION} -D PYODIDE_SOURCE=${PYODIDE_SOURCE} -D JUPYTERLITE_VERSION=${JUPYTERLITE_VERSION} -DCMAKE_MAKE_PROGRAM=/usr/bin/make " 
-docker run  ${DOCKEROPT} -w /home/docker/src --pull==never -v "$PWD":"/home/docker/src"   tradingstrategy:jupyterlite bash -i -c "export MAKE=make && export PYODIDE_JOBS=10 && make -j 10"
+docker run  ${DOCKEROPT} -w /home/docker/src --pull==never -v "$PWD":"/home/docker/src"  tradingstrategy:jupyterlite bash  -c "export MAKE=make && cmake . -D PYODIDE_VERSION=${PYODIDE_VERSION} -D PYODIDE_SOURCE=${PYODIDE_SOURCE} -D JUPYTERLITE_VERSION=${JUPYTERLITE_VERSION} -DCMAKE_MAKE_PROGRAM=/usr/bin/make " 
+docker run  ${DOCKEROPT} -w /home/docker/src --pull==never -v "$PWD":"/home/docker/src"   tradingstrategy:jupyterlite bash  -c "export MAKE=make && export PYODIDE_JOBS=10 && make -j 10"
 if [ -t 0 ] ; then
   docker run  ${DOCKEROPT} -w /home/docker/src --pull==never -v "$PWD":"/home/docker/src"   tradingstrategy:jupyterlite bash    
 fi
