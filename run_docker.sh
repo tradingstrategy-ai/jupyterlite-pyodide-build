3ea2e1db45955cec5c0d8ecf1cc821563ed1064f
Joe Marshall <joe.marshall@nottingham.ac.uk>
2022-07-19 13:51:50 +0100

    yet another attempt to get docker run on github

--- run_docker.sh
+++ run_docker.sh
@@ -17,7 +17,7 @@ docker build . --build-arg base_image=${DEFAULT_PYODIDE_DOCKER_IMAGE} -t trading
 #    DOCKEROPT=
 #fi
 
-docker run  ${DOCKEROPT} -w /home/docker/src --pull==never -v "$PWD":"/home/docker/src"  tradingstrategy:jupyterlite sudo su docker -lc "cd src && export MAKE=make && cmake . -D PYODIDE_VERSION=${PYODIDE_VERSION} -D PYODIDE_SOURCE=${PYODIDE_SOURCE} -D JUPYTERLITE_VERSION=${JUPYTERLITE_VERSION} -DCMAKE_MAKE_PROGRAM=/usr/bin/make " 
+sudo docker run  ${DOCKEROPT} -w /home/docker/src --pull==never -v "$PWD":"/home/docker/src"  tradingstrategy:jupyterlite bash -c "cd /home/docker/src && export MAKE=make && cmake . -D PYODIDE_VERSION=${PYODIDE_VERSION} -D PYODIDE_SOURCE=${PYODIDE_SOURCE} -D JUPYTERLITE_VERSION=${JUPYTERLITE_VERSION} -DCMAKE_MAKE_PROGRAM=/usr/bin/make " 
 #docker run  ${DOCKEROPT} -w /home/docker/src --pull==never -v "$PWD":"/home/docker/src"   tradingstrategy:jupyterlite su docker -lc "export MAKE=make && export PYODIDE_JOBS=10 && make -j 10"
 #if [ -t 0 ] ; then
 #  docker run  ${DOCKEROPT} -w /home/docker/src --pull==never -v "$PWD":"/home/docker/src"   tradingstrategy:jupyterlite bash    
