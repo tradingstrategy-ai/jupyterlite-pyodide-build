b8dd073b1475848557fde369274e15f165a7dac5
Joe Marshall <joe.marshall@nottingham.ac.uk>
2022-07-19 13:37:45 +0100

    testing CI commands

--- run_docker.sh
+++ run_docker.sh
@@ -10,15 +10,15 @@ DEFAULT_PYODIDE_DOCKER_IMAGE="${PYODIDE_IMAGE_REPO}/pyodide-env:${PYODIDE_IMAGE_
 PYODIDE_VERSION=${PYODIDE_PREBUILT_IMAGE_TAG}
 PYODIDE_SOURCE=https://github.com/pyodide/pyodide.git
 JUPYTERLITE_VERSION="0.1.0b9"
-DOCKEROPT=-t
+#DOCKEROPT=-dt
 
 docker build . --build-arg base_image=${DEFAULT_PYODIDE_DOCKER_IMAGE} -t tradingstrategy:jupyterlite && \
-if [ -t 0 ] ; then
-    DOCKEROPT=-it
-fi
+#if [ -t 0 ] ; then
+#    DOCKEROPT=
+#fi
 
-docker run  ${DOCKEROPT} -w /home/docker/src --pull==never -v "$PWD":"/home/docker/src"  tradingstrategy:jupyterlite bash  -c "export MAKE=make && cmake . -D PYODIDE_VERSION=${PYODIDE_VERSION} -D PYODIDE_SOURCE=${PYODIDE_SOURCE} -D JUPYTERLITE_VERSION=${JUPYTERLITE_VERSION} -DCMAKE_MAKE_PROGRAM=/usr/bin/make " 
-docker run  ${DOCKEROPT} -w /home/docker/src --pull==never -v "$PWD":"/home/docker/src"   tradingstrategy:jupyterlite bash  -c "export MAKE=make && export PYODIDE_JOBS=10 && make -j 10"
-if [ -t 0 ] ; then
-  docker run  ${DOCKEROPT} -w /home/docker/src --pull==never -v "$PWD":"/home/docker/src"   tradingstrategy:jupyterlite bash    
-fi
+docker run  ${DOCKEROPT} -w /home/docker/src --pull==never -v "$PWD":"/home/docker/src"  tradingstrategy:jupyterlite sudo su docker -lc "cd src && export MAKE=make && cmake . -D PYODIDE_VERSION=${PYODIDE_VERSION} -D PYODIDE_SOURCE=${PYODIDE_SOURCE} -D JUPYTERLITE_VERSION=${JUPYTERLITE_VERSION} -DCMAKE_MAKE_PROGRAM=/usr/bin/make " 
+#docker run  ${DOCKEROPT} -w /home/docker/src --pull==never -v "$PWD":"/home/docker/src"   tradingstrategy:jupyterlite su docker -lc "export MAKE=make && export PYODIDE_JOBS=10 && make -j 10"
+#if [ -t 0 ] ; then
+#  docker run  ${DOCKEROPT} -w /home/docker/src --pull==never -v "$PWD":"/home/docker/src"   tradingstrategy:jupyterlite bash    
+#fi
