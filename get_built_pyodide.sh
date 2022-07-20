33fd3e4cd0a9670da41139e0a4222e12c0fd9de9
Joe Marshall <joe.marshall@nottingham.ac.uk>
2022-07-20 10:28:14 +0100

    faster make for CI

--- get_built_pyodide.sh
+++ get_built_pyodide.sh
@@ -5,4 +5,13 @@ for pname in pyodide/packages/*/; do
     mkdir -p ${pname}/build
     echo > ${pname}/build/.packaged
 done
+# numpy needs building into packages/.artifacts in pyodide for pyarrow to build - 
+# don't need to do anything else with the build here 
+rm pyodide/packages/numpy/build/.packaged
+
+export PYODIDE_ROOT=`realpath pyodide`
+
+pushd pyodide/pyodide-build
+python -m pyodide_build buildpkg ../packages/numpy/meta.yaml
 echo "Got prebuilt pyodide packages and marked as built"
+popd
