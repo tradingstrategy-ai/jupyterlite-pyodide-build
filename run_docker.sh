c5a78f8e1efba765d334fd8a4e4063c8c8bcc086
Joe Marshall <joe.marshall@nottingham.ac.uk>
2022-07-20 15:55:01 +0100

    checkout submodule

--- run_docker.sh
+++ run_docker.sh
@@ -8,7 +8,7 @@ if [ -t 0 ] ; then
     bash ./build_docker.sh
     DOCKEROPT=-it
 fi
-
+git submodule update --init --recursive
 
 docker run  ${DOCKEROPT} -w /home/docker/src --pull==never -v "$PWD":"/home/docker/src"  tradingstrategy:jupyterlite bash -c "export PYODIDE_ROOT=/home/docker/src/pyodide && cd /home/docker/src && export MAKE=make && cmake . -D PYODIDE_VERSION=${PYODIDE_VERSION} -D PYODIDE_SOURCE=${PYODIDE_SOURCE} -D JUPYTERLITE_VERSION=${JUPYTERLITE_VERSION} -DCMAKE_MAKE_PROGRAM=/usr/bin/make " 
 docker run  ${DOCKEROPT} -w /home/docker/src --pull==never -v "$PWD":"/home/docker/src"   tradingstrategy:jupyterlite bash -c "export PYODIDE_ROOT=/home/docker/src/pyodide && export MAKE=make && export PYODIDE_JOBS=10 && make -j 10"
