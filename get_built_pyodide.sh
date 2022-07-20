2a31f53af1f69723c96e43c9835561f2123193ba
Joe Marshall <joe.marshall@nottingham.ac.uk>
2022-07-20 15:01:39 +0100

    CI fixes

--- get_built_pyodide.sh
+++ get_built_pyodide.sh
@@ -11,9 +11,17 @@ with open("pyodide/build/packages.json","r") as f:
   for package,info in plist['packages'].items():
     if os.path.exists(f"pyodide/packages/{info['name']}/meta.yaml"):
         print(f"Making dist for {package}")
-        os.makedirs(f"pyodide/packages/{info['name']}/build/dist",exist_ok=True)
-        shutil.copyfile(f"pyodide/build/{info['file_name']}",f"pyodide/packages/{info['name']}/build/dist/{info['file_name']}")
+        os.makedirs(f"pyodide/packages/{info['name']}/dist",exist_ok=True)
+        os.makedirs(f"pyodide/packages/{info['name']}/build",exist_ok=True)
+        shutil.copyfile(f"pyodide/build/{info['file_name']}",f"pyodide/packages/{info['name']}/dist/{info['file_name']}")
         open(f"pyodide/packages/{info['name']}/build/.packaged", 'w'). close()
 EOF
 
+# mark everything as built, to catch library packages
+for pname in pyodide/packages/*/; do
+    mkdir -p ${pname}/build
+    echo > ${pname}/build/.packaged
+done
+# force numpy rebuild
+rm pyodide/packages/numpy/build/.packaged
 
