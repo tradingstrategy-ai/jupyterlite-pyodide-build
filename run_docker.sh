cd8a456eaad12872f9ff24f4b4c7f209b6376735
Joe Marshall <joe.marshall@nottingham.ac.uk>
2022-07-21 10:12:00 +0100

    set UID/GID to same as current user

--- run_docker.sh
+++ run_docker.sh
@@ -10,6 +10,10 @@ if [ -t 0 ] ; then
 fi
 git submodule update --init --recursive
 
+docker run  ${DOCKEROPT} -w /home/docker/src --pull==never tradingstrategy:jupyterlite bash -c "sudo usermod -u $(id -u ${USER}) docker"
+docker run  ${DOCKEROPT} -w /home/docker/src --pull==never tradingstrategy:jupyterlite bash -c "sudo groupmod -g $(id -g ${USER}) docker"
+docker run  ${DOCKEROPT} -w /home/docker/src --pull==never tradingstrategy:jupyterlite bash -c "echo UID/GID set to \$(id)"
+
 docker run  ${DOCKEROPT} -w /home/docker/src --pull==never -v "$PWD":"/home/docker/src"  tradingstrategy:jupyterlite bash -c "export PYODIDE_ROOT=/home/docker/src/pyodide && cd /home/docker/src && export MAKE=make && cmake . -D PYODIDE_VERSION=${PYODIDE_VERSION} -D PYODIDE_SOURCE=${PYODIDE_SOURCE} -D JUPYTERLITE_VERSION=${JUPYTERLITE_VERSION} -DCMAKE_MAKE_PROGRAM=/usr/bin/make " 
 docker run  ${DOCKEROPT} -w /home/docker/src --pull==never -v "$PWD":"/home/docker/src"   tradingstrategy:jupyterlite bash -c "export PYODIDE_ROOT=/home/docker/src/pyodide && export MAKE=make && export PYODIDE_JOBS=10 && make -j 10"
 if [ -t 0 ] ; then
